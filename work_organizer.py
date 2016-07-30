8fba5fffcaa06e05c69b976511dceb4f7cc7c7f9
Luis Cazares <Luis Cazares>
2016-07-29 23:06:03 -0500

    can run organizer without input

--- work_organizer.py
+++ work_organizer.py
@@ -4,45 +4,54 @@ import shutil
 import re
 
 
-def organizer(folder):
-    if folder.lower() == "main":
-        os.chdir('C:/Users/luisc/Documents/Scan')
-    elif folder.lower() == "orders":
-        os.chdir('E:/Orders')
-    elif folder.lower() == "checks":
-        os.chdir('E:/Checks')
-    elif folder.lower() == "timesheets":
-        os.chdir('E:/Timesheets')
-    else:
-        print ("that is not a valid option")
-        return
+def organizer():
+    counter = 0
+    # Start in Scan and move to doc drive.
+    os.chdir('C:/Users/luisc/Documents/Scan')
+    sorter(counter)
+    counter += 1    
+    # Move to Orders into specific order folder
+    os.chdir('E:/Orders')
+    sorter(counter)
+    counter += 1
+    # Move Checks into specific company folder
+    os.chdir('E:/Check')
+    sorter(counter)
+    counter += 1
+    # Move Timesheets into specific driver folder
+    os.chdir('E:/Timesheet')
+    sorter(counter)
+    return
 
+def sorter(counter):
     for file in glob.glob("*"):
         split_file = file.split(" ")
         if not os.path.isdir(file):
-            if folder.lower() == "main":
+            if counter == 0:
                 ref = split_file[0]
-            elif folder.lower() == "orders":
+            elif counter == 1:
                 ref = split_file[0]
-            elif folder.lower() == "checks":
-                ref = split_file[1]
-            elif folder.lower() == "timesheets":
-                ref = "{0} {1}".format(split_file[1], split_file[2])
+            elif counter == 1:
+                ref = split_file[2]
+            elif counter == 2:
+                ref = "{0} {1}".format(split_file[2], split_file[3])
             else:
                 print ("{0} {1}".format(file, "is not a valid file"))
                 return
 
             source = "{0}/{1}".format(os.path.dirname(os.path.realpath("__file__")), file)
-            print (source)
-            if folder.lower() == "main":
+
+            if counter == 0:
                 if re.match(r'^[0-9]\w', ref):
                     destination = "E:/Orders/{0}".format(file)
             else:    
                 destination = "{0}/{1}/{2}".format(os.path.dirname(os.path.realpath("__file__")), ref, file)
-                print (destination) 
+
             # move file or create folder then move file
             if not re.match(r'^[0-9]\w', ref):
                 if not os.path.exists(ref):
                     os.makedirs(ref)
             shutil.move(source, destination)
-            
+
+if __name__ == '__main__':
+    organizer()
