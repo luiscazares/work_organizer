d5fa676f03758b388731a22354fd13eec08410c8
Luis Cazares <me@luiscazares.com>
2019-01-10 08:59:50 -0600

    Added receipt sorting into organizer by year and month

--- work_organizer.py
+++ work_organizer.py
@@ -2,12 +2,16 @@ import glob
 import os
 import shutil
 import re
+import calendar
 
 
 def sorter(iteration):
     """
     Sorter is responsible for setting source and destination variables for files in drop folder
     """
+    # Initializing year for non use.
+    year = ""
+
     for file in glob.glob("*"):
         split_file = file.split(" ")
         if not os.path.isdir(file):
@@ -21,6 +25,14 @@ def sorter(iteration):
             # Iteration at timesheet, grab name of driver
             elif iteration == 3:
                 ref = "{0} {1}".format(split_file[2], split_file[3])
+            # Iteration at receipts, grab month
+            elif iteration == 4:
+                # Will grab month from date on file name ex'['ER0001', '1-1-18']'
+                date = split_file[1].split("-")
+                year = "20" + date[2].split(".")[0]
+                month = int(date[0])
+                ref = calendar.month_name[month]
+
             else:
                 return
 
@@ -37,38 +49,61 @@ def sorter(iteration):
                     destination = "E:/Expense Report/{0}".format(file)
                 else:
                     destination = "E:/{0}/{1}".format(ref, file)
+            elif iteration == 4:
+                destination = "{0}/{1}/{2}/{3}".format(os.path.dirname(os.path.realpath("__file__")), year, ref, file)
             else:
                 destination = "{0}/{1}/{2}".format(os.path.dirname(os.path.realpath("__file__")), ref, file)
 
             # move file or create folder then move file
-            mover(source, destination, ref)
+            mover(source, destination, ref, year)
             
 
-def mover(source, destination, ref):
+def mover(source, destination, ref, year):
     """
     Mover uses pass variables to move files
     """
+    # Move to corresponding parent folder
     if iteration == 0:
         shutil.move(source, destination)
+    # When pertaining to Receipts or Expense reports.
+    elif iteration == 4:
+        # Check if year folder exists. If not, create it.
+        if not os.path.exists(year):
+            os.makedirs(year)
+        # Check if month of year folder exists. If not, create it.
+        if not os.path.exists(year + "/" + ref):
+            os.makedirs(year + "/" + ref)
+        # Move to year/month
+        shutil.move(source, destination)
     else:
+        # Check if folder for reference exists. If not create it.
         if not os.path.exists(ref):
             os.makedirs(ref)
         shutil.move(source, destination)
 
 if __name__ == '__main__':
     iteration = 0
+
     # Start in Scan and move files to doc drive.
     os.chdir('C:/Users/luisc/Documents/Scan')
     sorter(iteration)
-    iteration += 1    
+    iteration += 1
+
     # Move to Orders into specific order folder
     os.chdir('E:/Orders')
     sorter(iteration)
     iteration += 1
+
     # Move Checks into specific company folder
     os.chdir('E:/Check')
     sorter(iteration)
     iteration += 1
+
     # Move Timesheets into specific driver folder
     os.chdir('E:/Timesheet')
+    sorter(iteration)
+    iteration += 1
+
+    # Move Receipts into specific Year/month folder
+    os.chdir('E:/Receipts')
     sorter(iteration)
